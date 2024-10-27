7f4d1267be03284cffa694218416e3b7434db997
4bh16am <45392298+abhi123vj@users.noreply.github.com>
2024-10-27 22:56:33 +0530

    logout api added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div className="flex ">
-      <Button variant="primary">Primary</Button>
-      <Button variant="secondary">secondary</Button>
-      <Button variant="destructive">destructive</Button>
-      <Button variant="ghost">ghost</Button>
-      <Button variant="muted">muted</Button>
-      <Button variant="tertiary">tertiary</Button>
-    </div>
-  );
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+  const { mutate }= useLogout();
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-sin");
+    }
+  });
+  return <div className="flex flex-col justify-center ">
+    Authorized user
+    <Button className="w-2/12" onClick={() => mutate()}>
+      Logout
+    </Button>
+  </div>;
 }
